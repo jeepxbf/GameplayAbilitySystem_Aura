5b1f6d9646be4e83d0ac61fc753ddd39ed2bd06d
496003921@qq.com <496003921@qq.com>
2025-06-13 17:57:07 +0800

    28-3_-_Globe_Progress_Bar_cn-480P_标清-AVC 创建了一个血条进度条

--- Source/Aura/Public/UI/Widget/AuraUserWidget.h
+++ Source/Aura/Public/UI/Widget/AuraUserWidget.h
@@ -18,7 +18,7 @@ public:
 	void SetWidgetController(UObject* InWidgetController);
 	
 	UPROPERTY(BlueprintReadOnly)
-	TObjectPtr<UObject*> WidgetController;
+	TObjectPtr<UObject> WidgetController;
 protected:
 	//BlueprintImplementableEvent需要在蓝图中实现，不能在c++中实现，但是可以在c++中调用
 	UFUNCTION(BlueprintImplementableEvent)
